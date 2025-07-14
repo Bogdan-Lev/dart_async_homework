ef30a1e059f373e2abeeff96c7178c87b861d4ed
Bogdan Lev <dartpab@gmail.com>
2025-07-14 22:57:36 +0300

    feat(DART-11): add task 7 сountdown from stream (periodic)

--- dart_async_homework/lib/homeworks/lesson_11/hw_streams.dart
+++ dart_async_homework/lib/homeworks/lesson_11/hw_streams.dart
@@ -1,6 +1,7 @@
 // Частина 2: Streams
 void main() async {
   await runTask6();
+  await runTask7();
 }
 
 // Task 6: Стрім з чисел (fromIterable)
@@ -33,4 +34,24 @@ Future<void> runTask6() async {
       .asFuture();
 
   print('-------------------  End_Task_6  -------------------');
+}
+
+// Task 7: Зворотний відлік зі стріму (periodic)
+// 1.Створіть Stream<int> за допомогою Stream.periodic, який генерує числа з затримкою в 1 секунду.
+// 2.Обмежте відлік до 10 чисел за допомогою методу take.
+// 3.Виведіть кожне число у консоль у форматі: "1...", "2...", "3...", ... "10...".
+
+Future<void> runTask7() async {
+  print('------------------- Start_Task_7 -------------------');
+
+  final countdownStream = Stream<int>.periodic(
+    Duration(seconds: 1),
+    (count) => count + 1,
+  ).take(10);
+
+  await for (final number in countdownStream) {
+    print('$number...');
+  }
+
+  print('-------------------  End_Task_7  -------------------');
 }
